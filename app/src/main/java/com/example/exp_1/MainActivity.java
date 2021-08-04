1710802b5801000cfc59123c175e5e3676f58b51
Praveenreddy2806 <vangalapraveen@karunya.edu.in>
2021-08-04 10:32:12 +0530

    completed

--- app/src/main/java/com/example/exp_1/MainActivity.java
+++ app/src/main/java/com/example/exp_1/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
      EditText Height, Weight;
      Button calc;
-     TextView bmi, op;
+     TextView bmi, res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,16 +21,40 @@ public class MainActivity extends AppCompatActivity {
         Height = findViewById(R.id.height);
         Weight = findViewById(R.id.weight);
         bmi = findViewById(R.id.bmi);
-        op = findViewById(R.id.output);
+        res = findViewById(R.id.output);
         calc = findViewById(R.id.calc);
 
         calc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int H = Integer.parseInt(Height.getText().toString());
-                int W = Integer.parseInt(Weight.getText().toString());
-                String  res = String.valueOf(((W/H)/H)*10000);
-                Toast.makeText(MainActivity.this, "res :"+res, Toast.LENGTH_SHORT).show();
+                float H = Float.parseFloat((Height.getText().toString()));
+                float W = Float.parseFloat((Weight.getText().toString()));
+                float result = (W/(H*H))*10000;
+                String BMI = String.valueOf(result);
+                if (BMI == ""){
+                    Toast.makeText(MainActivity.this, "Please Enter Height and Weight", Toast.LENGTH_SHORT).show();
+                }else {
+                    bmi.setText(BMI);
+                    if(result<16){
+                        res.setText("Severe Thinness");
+                    }
+                    else if(result >=16 && result <=17){
+                        res.setText("Moderate Thickness");
+                    }else if(result>17 && result <19){
+                        res.setText("Mild Thickness");
+                    }else if(result>18 && result<=25){
+                        res.setText("Normal Thickness");
+                    }else if (result>25 && result<=30){
+                        res.setText("Over Weight");
+                    }else if (result>30 && result<=35){
+                        res.setText("Obese Class 1");
+                    }else if (result>35 && result<=40){
+                        res.setText("Obese Class 2");
+                    }else if (result > 40){
+                        res.setText("Obese Class 3");
+                    }
+                }
+
             }
         });
 
